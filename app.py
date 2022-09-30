2b66e408efe4400261173645a375e1f32955162b
Rudra Pratap Singh <65117190+Rudraps07@users.noreply.github.com>
2022-09-30 21:26:49 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
 import pickle
-from Flask import Flask,request,app.jsonify,url_for,render_templates
+from Flask import Flask,request,app,jsonify,url_for,render_templates
+import numpy as np
+import pandas as pd
+
+##Load the model
+app=Flask(__name__)
+model=pickel.load(open('Boston_housing_data\model.pkl','rb'))
+
+@app.route('/') 
+def home():
+    return render_templates('home.html')
+    
